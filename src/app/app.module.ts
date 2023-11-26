457d3b19cf080700bb3f5d7449a55473ad2fe6fa
Mak <aelacita@gmail.com>
2023-11-26 16:40:53 +0800

    other files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,45 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { Router, RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+
+import { AngularFireModule } from '@angular/fire/compat';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignupComponent,
+    LoginComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,ReactiveFormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    AngularFireModule.initializeApp(environment.firebase),
+    BrowserAnimationsModule,
+    provideAuth(() => getAuth()),
+    MatButtonModule, MatCardModule, MatInputModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'fir-ngcrud',
+    appId: '1:1042136259680:web:f979f8674aec7fa0c2b705',
+    storageBucket: 'fir-ngcrud.appspot.com',
+    apiKey: 'AIzaSyCyV4klnMhYjzA-22H9pBeoNGGSRKFiQmI',
+    authDomain: 'fir-ngcrud.firebaseapp.com',
+    messagingSenderId: '1042136259680',
+  }
+}; 
